604197a0c80987ba126a5c24c32d4ea14480e579
dvctrl <davidmouzinho.contato@gmail.com>
2023-05-11 21:21:50 -0300

    colors e splash activity

--- app/src/main/java/br/com/etecia/myapp/SplashActivity.java
+++ app/src/main/java/br/com/etecia/myapp/SplashActivity.java
@@ -2,7 +2,9 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_splash);
+
+        // criando o carregamento da janela de login
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //abrindo a janela de login
+                  startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                  //fechando a janela atual
+                  finish();
+            }
+        },3000);
+
     }
 }
