1c466d4d7d1fb8db8624ca408e79185f3d9a5e3e
dvctrl <davidmouzinho.contato@gmail.com>
2023-05-18 20:24:36 -0300

    menu page

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -2,13 +2,57 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 
 public class LoginActivity extends AppCompatActivity {
+    Button btnEntrar, btnSair;
+    TextInputEditText txtLogin, txtSenha;
+    TextView lblcadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_login);
+
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnSair = findViewById(R.id.btnSair);
+        txtLogin = findViewById(R.id.txtLogin);
+        txtSenha = findViewById(R.id.txtSenha);
+        lblcadastrar = findViewById(R.id.lblCadastrar);
+
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usuario, senha;
+
+                usuario = txtLogin.getText().toString();
+                senha = txtLogin.getText().toString();
+
+                if (usuario.equals("etecia") && senha.equals("etecia")) {
+                    startActivity(new Intent(getApplicationContext(), MenuActivity.class));
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Usuário ou senha inválidos",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
