a42aebf995051b4148809f49cbcac7273a54a975
dvctrl <davidmouzinho.contato@gmail.com>
2023-05-18 22:11:32 -0300

    menu activity

--- app/src/main/java/br/com/etecia/myapp/MenuActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MenuActivity.java
@@ -2,6 +2,7 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -9,25 +10,38 @@ import com.google.android.material.card.MaterialCardView;
 
 public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
 
-    MaterialCardView MenuPets, MenuLoja, MenuDoacao, MenuServicos,MenuParceiros, MenuLocalizar;
+    MaterialCardView idMenuPets, MenuLoja, MenuDoacao, MenuServicos, MenuParceiros, MenuLocalizar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_menu);
 
-        MenuPets = findViewById(R.id.MenuPets);
+        idMenuPets = findViewById(R.id.MenuPets);
         MenuLoja = findViewById(R.id.MenuLoja);
-        MenuDoacao= findViewById(R.id.MenuDoacao);
+        MenuDoacao = findViewById(R.id.MenuDoacao);
         MenuServicos = findViewById(R.id.MenuServicos);
         MenuParceiros = findViewById(R.id.MenuParceiros);
         MenuLocalizar = findViewById(R.id.MenuLocalizar);
 
+        idMenuPets.setOnClickListener(this);
+        MenuLoja.setOnClickListener(this);
+        MenuDoacao.setOnClickListener(this);
+        MenuServicos.setOnClickListener(this);
+        MenuParceiros.setOnClickListener(this);
+        MenuLocalizar.setOnClickListener(this);
+
 
     }
+
 
     @Override
     public void onClick(View v) {
-
+        if (v.getId() == R.id.MenuPets) {
+            startActivity(new Intent(getApplicationContext(), PetsActivity.class));
+        } else if (v.getId() == R.id.MenuLoja) {
+            startActivity(new Intent(getApplicationContext(), AlimentosActivity.class));
+        }
     }
 }
+
